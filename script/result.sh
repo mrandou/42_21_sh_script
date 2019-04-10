632a70b7027f4b49e781969434de7d62620fd52e
Maxime RANDOU <mrandou@e1r5p15.42.fr>
2019-04-10 13:37:04 +0200

    fix++

--- script/result.sh
+++ script/result.sh
@@ -5,11 +5,13 @@ cat ./files/output/output_bash
 printf "\n\033[32m\033[3m21/42sh output : \n\033[0m"
 cat ./files/output/output_2142sh
 printf "\n\033[32m\033[3mDiff Results : \n\033[0m"
+printf "\033[31m"
 cat ./files/output/result.txt
 
-nb=`cat ./files/output/result.txt | grep -c \"\"`
-if [ $nb -eq 0 ]
+nb=`cat ./files/output/result.txt | grep -c ""`
+if [ "$nb" = 0 ]
 then
 	echo "No differents, everything is fine !"
 fi
+printf "\033[0m"
 exit 0
